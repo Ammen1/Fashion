95bcfd203ca701a8f47075849e2445cb70aa4ea6
Amen <amenguda@gmail.com>
2023-01-06 01:06:33 +0300

    importing

--- account/views.py
+++ account/views.py
@@ -1,3 +1,21 @@
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.template.loader import render_to_string
+from django.urls import reverse
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from orders.models import Order
+from orders.views import user_orders
+# from store.models import Product
+
+from .form import RegistrationForm, UserAddressForm, UserEditForm
+from .models import Address, Customer
+from .tokens import account_activation_token
+
 from django.shortcuts import render, redirect, reverse
 from django.conf import settings
 
